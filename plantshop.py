45c889ae95cd486e12f64102d320441a1bdd57d4
finben <benfindeisen@gmail.com>
2020-11-23 17:50:19 -0600

    adding new functionality to the plant to wither

--- plantshop.py
+++ plantshop.py
@@ -3,6 +3,14 @@ class Plant:
         self.number_of_leaves = number_of_leaves
         self.root_system = root_system
 
-rose = Plant(7,True)
-cactus = Plant(0,True)
+    def wither(self, how_many_leaves_fall):
+        self.number_of_leaves -= how_many_leaves_fall
+
+
+rose = Plant(7, True)
+cactus = Plant(0, True)
+
+rose.wither(5)
+
+
 print(rose.number_of_leaves)

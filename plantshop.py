6e44d3736702c58232f158afa412a54ddc95e229
cielopaz <cielo.cerezo@toyota.com>
2020-11-23 18:10:28 -0600

    added growth

--- plantshop.py
+++ plantshop.py
@@ -1,16 +1,24 @@
 class Plant:
-    def __init__(self, number_of_leaves, root_system):
+    def __init__(self, number_of_leaves, root_system, growth_in_inches):
         self.number_of_leaves = number_of_leaves
         self.root_system = root_system
+        self.growth_in_inches = growth_in_inches
+    
+    def grow(self, no_inches):
+        self.growth_in_inches = no_inches
 
     def wither(self, how_many_leaves_fall):
         self.number_of_leaves -= how_many_leaves_fall
 
+        
+        
 
-rose = Plant(7, True)
-cactus = Plant(0, True)
+rose = Plant(7, True,10)
+cactus = Plant(0, True,20)
 
 rose.wither(5)
+rose.grow(15)
+
+print(rose.growth_in_inches)
 
 
-print(rose.number_of_leaves)

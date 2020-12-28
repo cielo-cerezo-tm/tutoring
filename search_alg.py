ce5c2a787a5d9a0a368906e99f0ec2babd69b9a9
cielopaz <cielo.cerezo@toyota.com>
2020-12-28 13:32:43 -0600

    for loop

---
+++ search_alg.py
@@ -0,0 +1,20 @@
+#import datetime
+# x = datetime.datetime.now()
+#print(x)
+# x = datetime.datetime(2020, 12, 25)
+# print (x)
+#Function Definition
+xmas_meal = ['vegeatbles', 'vegeatbles', 'yule_log_cake']
+def search (main):
+    #print (main)
+    for item in xmas_meal:
+        if item == (main):
+            print('found')
+   
+
+
+search('vegeatbles')
+
+
+
+
